1b973f21911b5999915aaa63c8327cfe805239fb
Vedant336Neekhra <152647260+Vedant336Neekhra@users.noreply.github.com>
2024-07-07 04:47:52 +0530

    Update CLIP_for_FICE.py

--- CLIP_for_FICE/CLIP_for_FICE.py
+++ CLIP_for_FICE/CLIP_for_FICE.py
@@ -25,10 +25,9 @@ def CLIP_encode_image(image_list, raw_image:bool=False):
             img_tensors.append(preprocess(img))
         img_input = torch.Tensor(np.stack(img_tensors)).to(device)
     else:
-        transform = TF.Compose([
-            TF.Resize(size=224, interpolation=TF.InterpolationMode.BICUBIC, max_size=None, antialias=True),
-            TF.CenterCrop(size=(224,224)),
-            TF.Normalize([0.48145466, 0.4578275, 0.40821073], [0.26862954, 0.26130258, 0.27577711])
+        transform = T.Compose([
+            T.Resize((224, 224)),  # Ensure the image is resized to 224x224 pixels
+            T.Normalize(mean=[0.481, 0.457, 0.408], std=[0.268, 0.261, 0.275])  # Normalize with CLIP's mean and std
         ])
         img_input = transform(image_list).to(device)
         # img_input = image_list.to(device)
