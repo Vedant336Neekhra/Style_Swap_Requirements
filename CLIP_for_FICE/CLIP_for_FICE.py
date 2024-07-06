e4609b53dd05be6fc0ecb78b7fd349ea87ee446e
Vedant336Neekhra <152647260+Vedant336Neekhra@users.noreply.github.com>
2024-07-07 02:56:16 +0530

    Add files via upload

---
+++ CLIP_for_FICE/CLIP_for_FICE.py
@@ -0,0 +1,43 @@
+# importing necessay libraries
+import torch
+import torchvision.transforms as TF
+import numpy as np
+import clip
+
+clip_model, preprocess = clip.load('RN50')
+clip_model.eval()
+
+def CLIP_encode_text(text_list:list) -> torch.Tensor:
+    # Encodes given list of tokens and return the text embeddings in the shape of (batch_size, 512)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    txt_tokens = clip.tokenize(text_list).to(device)
+    txt_features = clip_model.encode_text(txt_tokens).float()
+    txt_features_ = txt_features/txt_features.norm(dim=-1, keepdim=True)
+    return txt_features_
+
+def CLIP_encode_image(image_list, raw_image:bool=False):
+    # Encodes given list of images (or tensor) and return the image embeddings in the shape of (batch_size, 512)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # preprocess if rawimage
+    if raw_image:
+        img_tensors = []
+        for img in image_list:
+            img_tensors.append(preprocess(img))
+        img_input = torch.Tensor(np.stack(img_tensors)).to(device)
+    else:
+        transform = TF.Compose([
+            TF.Resize(size=224, interpolation=TF.InterpolationMode.BICUBIC, max_size=None, antialias=True)
+            # TF.CenterCrop(size=(224,224)),
+            # TF.Normalize([0.48145466, 0.4578275, 0.40821073], [0.26862954, 0.26130258, 0.27577711])
+        ])
+        img_input = transform(image_list).to(device)
+        # img_input = image_list.to(device)
+    img_features = clip_model.encode_image(img_input).float()
+    img_features_ = img_features/img_features.norm(dim=-1, keepdim=True)
+    return img_features_
+
+def CLIP_loss(text_list, image_list, raw_image:bool=False):
+    txt_embed = CLIP_encode_text(text_list)
+    img_embed = CLIP_encode_image(image_list, raw_image)
+    similarity = torch.mul(txt_embed, img_embed).sum(dim=-1)
+    return (1 - similarity).mean()
---
+++ segmentation_model.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.models.segmentation.deeplabv3 import deeplabv3_resnet50
+import cv2
+import torchvision.transforms as transforms
+
+
+class SegModel(nn.Module):  # todo: move to models
+    def __init__(self) -> None:
+        super().__init__()
+        self.model = deeplabv3_resnet50(pretrained=False, num_classes=3, pretrained_backbone=True)
+        ckpt = '/content/checkpoint/segmentator.pt'
+        ckpt = torch.load(ckpt, map_location='cpu')['state']
+        ckpt = {k: v for k, v in ckpt.items() if k != 'loss.weight'}
+        self.load_state_dict(ckpt)
+        self.eval().requires_grad_(False)
+
+    def forward(self, x):
+        x = self.model(x)['out']
+        x = F.softmax(x, dim=1)
+        
+        background = x[:,0].unsqueeze(1)
+        body = x[:,1].unsqueeze(1)
+        head = x[:,2].unsqueeze(1)
+        return background, body, head
+
+def for_model(image_path):
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    transform = transforms.Compose([
+        transforms.ToPILImage(),  # Convert the OpenCV image (numpy array) to a PIL image
+        transforms.Resize((256, 256)),  # Resize the image to 256x256
+        transforms.ToTensor()  # Convert the PIL image to a PyTorch tensor
+    ])
+    tensor_image = transform(image)
+    return tensor_image.view(-1,3,256,256)
+def semantic_loss(Ig,real_image_path,device):
+    model = SegModel()
+    model = model.to(device)
+    I = for_model(real_image_path)
+    I=I.to(device)
+    Ig=Ig.to(device)
+    with torch.no_grad():  
+      Sbg, Sbody, Shead = model(Ig)
+      SbgI, SbodyI, SheadI = model(I)
+    M = 1 - SbodyI
+    Limg = F.mse_loss(M*Ig, M*I, reduction='none')
+    Limg = Limg.mean((1,2,3)).sum()
+    Lhead = F.mse_loss(Shead, SheadI, reduction='none')
+    Lhead = Lhead.mean((1,2,3)).sum()
+    return Limg,Lhead
+
